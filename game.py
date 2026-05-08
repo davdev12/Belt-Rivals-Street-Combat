ea03c7e6590a4fc0b913c31ebf5d40482e59b896
davdev12 <davbot.github@gmail.com>
2026-05-08 21:40:21 +0200

    Scaled and renamed all frames

--- game.py
+++ game.py
@@ -5,11 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Belt Rivals: Street Combat")
 clock = pygame.time.Clock()
+bg_surface = pygame.image.load("images/background1.png")
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(bg_surface,(0, 0))
+
     pygame.display.update()
     clock.tick(60)
