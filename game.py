2fabdffb8f7339b43eb7acd6eb090714eda657f6
davdev12 <davbot.github@gmail.com>
2026-05-11 18:40:27 +0200

    Adds gravity and jumps for player

--- game.py
+++ game.py
@@ -5,7 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((901, 557))
 pygame.display.set_caption("Belt Rivals: Street Combat")
 clock = pygame.time.Clock()
-bg_surface = pygame.image.load("images/background1.png")
+
+bg_surface = pygame.image.load("images/background1.png").convert_alpha()
+
+skeleton_surf = pygame.image.load("images/skeleton_enemy/skeleton_passive_right.png").convert_alpha()
+skeleton_rect = skeleton_surf.get_rect(midbottom = (40, 530) )
+
+player_surf = pygame.image.load("images/BlueFist/passive_wide_left.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (700, 530))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +22,26 @@ while True:
             exit()
 
     screen.blit(bg_surface,(0, 0))
+
+
+    skeleton_rect.x += 2
+    if skeleton_rect.left > 900: skeleton_rect.right = 0
+
+
+    #player_rect.x -= 1
+    if player_rect.right < 0: player_rect.left = 901
+
+    screen.blit(skeleton_surf, skeleton_rect)
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 530: player_rect.bottom = 530
+    screen.blit(player_surf, player_rect)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and player_rect.bottom >= 530:
+        player_gravity = -26
+
 
     pygame.display.update()
     clock.tick(60)
